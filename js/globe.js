bc469193ab8e10600e3a072e615b7cbf05eb3138
Larsen Vallecillo <larsen@Weborican.com>
2025-01-07 12:17:14 -0600

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -7,33 +7,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const wiiglobe = document.querySelector('model-viewer');
 
   // Handles BGM based on users current time
-
-  // Function to determine which track to play
-  function timeofdayBGMplayer() {
-   
-      const currentHour = new Date().getHours();
-      const earthbgm = document.getElementById('BGMplayer');
-      let selectedTrack;
-
-      // Determine if it's day or night
-      if (currentHour >= 5 && currentHour < 18) {
-          // 50% chance to play day music or news
-          selectedTrack = Math.random() < 0.5 ? '/music/forecastday.mp3' : '/music/news.mp3';
-      } else {
-          // 50% chance to play night music or news
-          selectedTrack = Math.random() < 0.5 ? '/music/forecastnight.mp3' : '/music/news.mp3';
+  
+  document.addEventListener('DOMContentLoaded', () => {
+      // Function to determine which track to play
+      function playMusicBasedOnTime() {
+          const currentHour = new Date().getHours();
+          const audio = document.getElementById('audio');
+          let selectedTrack;
+          // Determine if it's day or night
+          if (currentHour >= 5 && currentHour < 18) {
+              // 50% chance to play day music or news
+              selectedTrack = Math.random() < 0.5 ? '/music/forecastday.mp3' : '/music/news.mp3';
+          } else {
+              // 50% chance to play night music or news
+              selectedTrack = Math.random() < 0.5 ? '/music/forecastnight.mp3' : '/music/news.mp3';
+          }
+          // Set the selected track to the audio element
+          audio.src = selectedTrack;
+          audio.loop = true; // Ensure the audio loops
+          audio.play();
       }
-
-      // Set the selected track to the audio element
-      earthbgm.src = selectedTrack;
-      earthbgm.play();
-
-  }
-
-  timeofdayBGMplayer();
-
-// Handles BGM based on users current time
-
+    
+      playMusicBasedOnTime();
+  });
 
     // SOUND EFFECTS
     
